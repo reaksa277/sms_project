b2d4f58d7f1922edebe5bfe68ccfb0b9915c9aa1
PICH2002 <hotsreypich@gmail.com>
2025-08-20 22:12:02 +0700

    feature Courses.java

---
+++ src/main/java/com/example/sms_project/entity/Courses.java
@@ -0,0 +1,25 @@
+package com.example.sms_project.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "courses")
+@Data
+public class Courses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String teacher_id;
+    private String hire_date;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}

21a458603fb21be69abcbc86778c56fb3c34bef8
reaksa <chhimchanreaksa@gmail.com>
2025-09-07 16:38:52 +0700

    refactor(enrollment): add listEnrollment in controller, toDto and toListDto in EnrollmentResponseDto and property

--- src/main/java/com/example/sms_project/controller/EnrollmentController.java
+++ src/main/java/com/example/sms_project/controller/EnrollmentController.java
@@ -2,19 +2,22 @@ package com.example.sms_project.controller;
 
 import com.example.sms_project.dto.enrollment.EnrollmentDto;
 import com.example.sms_project.model.BaseResponseModel;
+import com.example.sms_project.model.BaseResponseWithDataModel;
 import com.example.sms_project.service.EnrollmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping({"api/v1/enrollments"})
 public class EnrollmentController {
     @Autowired
     private EnrollmentService enrollmentService;
+
+    @GetMapping
+    public ResponseEntity<BaseResponseWithDataModel> listEnrollments() {
+        return enrollmentService.listEnrollments();
+    }
 
     @PostMapping
     public ResponseEntity<BaseResponseModel> createEnrollment(@RequestBody EnrollmentDto payload) {

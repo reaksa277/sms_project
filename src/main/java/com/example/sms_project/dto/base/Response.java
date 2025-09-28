c721dff53d64c1a01a1c671fe2552ccb763157d9
reaksa <chhimchanreaksa@gmail.com>
2025-09-28 14:36:27 +0700

    feat: add base response with timestamp dto

---
+++ src/main/java/com/example/sms_project/dto/base/Response.java
@@ -0,0 +1,55 @@
+package com.example.sms_project.dto.base;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+
+@JsonPropertyOrder(value = {"code", "message", "description", "date", "timestamp"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+public class Response {
+    private String code;
+    private String message;
+    private String description;
+    private Object data;
+    private Long timestamp;
+
+    private Response(String code, String message, String description, Object data) {
+        this.code = code;
+        this.message = message;
+        this.description = description;
+        this.data = data;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    private Response(String code, String message, String description) {
+        this.code = code;
+        this.message = message;
+        this.description = description;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public static Response success(String message,  String description) {
+        return new Response("200", message, description);
+    }
+
+    public static Response success(String code,  String message, String description) {
+        return new Response(code, message, description);
+    }
+
+    private static Response success(String code,  String message, String description, Object data) {
+        return new Response(code, message, description, data);
+    }
+
+    public static Response error(String code, String message, String description) {
+        return new Response(code, message, description);
+    }
+
+    public static Response error(String code, String message, String description, Object data) {
+        return new Response(code, message, description, data);
+    }
+
+    public static Response error(String message, String description) {
+        return new Response("400", message, description);
+    }
+}
